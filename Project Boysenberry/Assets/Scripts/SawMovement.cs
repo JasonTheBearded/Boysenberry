42d7de8bd421c3245cad13edba83e92d9652795b
JasonTheBearded <jpcavnar@gmail.com>
2017-12-31 16:13:56 -0600

    Revert "hazard updates"
    This reverts commit 1490337fff32b11a6ec55372559cb615c1e63f1e.

--- Project Boysenberry/Assets/Scripts/SawMovement.cs
+++ Project Boysenberry/Assets/Scripts/SawMovement.cs
@@ -4,60 +4,31 @@ using UnityEngine;
 
 public class SawMovement : MonoBehaviour {
 
-    public Vector3 target;
-    public float speed = 5f;
+	public Transform start;
+	public Transform target;
+	public float speed;
+	private bool starter;
 
-    private Vector3 start;
-    private bool complete;
-    private float step;
+	void Start(){
+		starter = true;
+	}
 
-	// Use this for initialization
-	void Start () {
-        start = transform.position;
-        step = speed * Time.deltaTime;
+	void Update(){
+		float step = speed * Time.deltaTime;
+
+		if (starter) {
+			MoveSawsForward(step);
+		}// else if (starter == false) {
+		//	ReturnSaws(step);
+		//}
+	}
+
+	void MoveSawsForward(float step){
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		starter = false;
 	}
 	
-	void Update () {
-        if (!complete)
-        {
-            StartCoroutine(StartSaws(target, step));
-        }
-
-        else
-        {
-            StartCoroutine(EndSaws(start, step));
-        }
-        
-        //transform.position = Vector3.MoveTowards(transform.position, start, step);
-    }
-
-    IEnumerator StartSaws(Vector3 newPosition, float step)
-    {
-        float sqrRemainingDistance = (transform.position - newPosition).sqrMagnitude;
-
-        //While that distance is greater than a very small amount (Epsilon, almost zero):
-        while (sqrRemainingDistance > float.Epsilon)
-        {
-            yield return new WaitForSeconds(.1f);
-            transform.position = Vector3.MoveTowards(transform.position, target, step);
-            sqrRemainingDistance = (transform.position - newPosition).sqrMagnitude;
-            yield return null;
-        }
-
-        complete = true;
-
-    }
-
-    IEnumerator EndSaws(Vector3 newPosition, float step)
-    {
-        float sqrRemainingDistance = (transform.position - newPosition).sqrMagnitude;
-
-        while (sqrRemainingDistance > float.Epsilon)
-        {
-            yield return new WaitForSeconds(.3f);
-            transform.position = Vector3.MoveTowards(transform.position, start, step);
-            sqrRemainingDistance = (transform.position - newPosition).sqrMagnitude;
-            yield return null;
-        }
-    }
+	//void ReturnSaws(float step){
+	//	transform.position = Vector3.MoveTowards (transform.position, start.position, step);
+	//}		
 }
