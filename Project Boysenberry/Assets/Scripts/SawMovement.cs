0e9ebe25a88515fd489268085679497a8ce23a86
JasonTheBearded <jpcavnar@gmail.com>
2017-12-30 22:34:23 -0600

    collider and script additions

---
+++ Project Boysenberry/Assets/Scripts/SawMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawMovement : MonoBehaviour {
+
+	public Transform start;
+	public Transform target;
+	public float speed;
+	private bool starter;
+
+	void Start(){
+		starter = true;
+	}
+
+	void Update(){
+		float step = speed * Time.deltaTime;
+
+		if (starter) {
+			MoveSawsForward(step);
+		}// else if (starter == false) {
+		//	ReturnSaws(step);
+		//}
+	}
+
+	void MoveSawsForward(float step){
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		starter = false;
+	}
+	
+	//void ReturnSaws(float step){
+	//	transform.position = Vector3.MoveTowards (transform.position, start.position, step);
+	//}		
+}
