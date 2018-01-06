b8d88b2deed685ad6038aed19ac2bbf6b16f7cad
JasonTheBearded <jpcavnar@gmail.com>
2018-01-06 01:37:47 -0600

    laser work

---
+++ Project Boysenberry/Assets/Scripts/LaserBloom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBloom : MonoBehaviour {
+
+    public float speed;
+    private float alpha = 0f;
+    private Color color;
+    private SpriteRenderer rend;
+
+    // Use this for initialization
+	void Start () {
+        rend = GetComponent<SpriteRenderer>();
+        color = rend.material.color;
+        color.a = 0f;
+        rend.color = color;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        while(alpha <= 1f)
+        {
+            Debug.Log("increasing alpha");
+            alpha += speed;
+            color.a = alpha;
+            rend.color = color;
+        }
+
+	}
+}
